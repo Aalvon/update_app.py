3c95b170ded77f07834d7ba1840299e7864b47e4
Aalvon <tenysonben333@gmail.com>
2026-07-03 10:54:39 +0530

    Update projectapp.py

--- projectapp.py
+++ projectapp.py
@@ -11,3 +11,18 @@ if'audio_path' not in st.session_state:
    st.session_state['audio_path'] = None
 if 'yt_error' in st.session_state:
   pass
+
+def cleanup_temp_files():
+     """Removes temporary files and resets memory."""
+     files = glob.glob("temp_*") + ["output_video.mp4"]
+     for f in files:
+          try:
+               os.remove(f)
+          except:
+              pass
+     st.session_state['audio_path'] = None
+     if 'yt_error' in st.session_state:
+         del st.session_state['yt_error']
+          
+                
+
